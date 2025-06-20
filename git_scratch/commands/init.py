cccc55c196d22873abdebf4dafc8c30f56bf2d94
stephanedescarpentries <stephane.descarpentries@hotmail.fr>
2025-06-20 20:42:45 +0200

    fix: add file param init cmd & feat: pytest for init cmd

--- git_scratch/commands/init.py
+++ git_scratch/commands/init.py
@@ -3,15 +3,18 @@ import typer
 
 app = typer.Typer()
 
-def init():
+def init(
+    folder: str = typer.Argument(None, help="Path to the folder where to initialize the repository")
+):
     """
     Initialize a new, empty pit repository.
     """
     # os.getcwd() = current path
-    git_dir = os.path.join(os.getcwd(), ".git")
+    target_dir = os.path.abspath(folder) if folder else os.getcwd()
+    git_dir = os.path.join(target_dir, ".git")
 
     if os.path.exists(git_dir):
-        typer.secho("Reinitialized existing Pit repository in {}/.git/".format(os.getcwd()), fg=typer.colors.YELLOW)
+        typer.secho("Reinitialized existing Pit repository in {}/.git/".format(target_dir), fg=typer.colors.YELLOW)
         raise typer.Exit()
 
     # Create basic Git structure
@@ -24,15 +27,16 @@ def init():
 
     # Create essential files
     with open(os.path.join(git_dir, "HEAD"), "w") as f:
-        f.write("ref: refs/heads/main\n")
+        f.write("ref: refs/heads/master\n")
     with open(os.path.join(git_dir, "config"), "w") as f:
         f.write(
             "[core]\n"
             "\trepositoryformatversion = 0\n"
             "\tfilemode = true\n"
             "\tbare = false\n"
+            "\tlogallrefupdates = true\n"
         )
     with open(os.path.join(git_dir, "description"), "w") as f:
         f.write("Unnamed pit repository; edit this file 'description' to name the repository.\n")
 
-    typer.secho("Initialized empty Pit repository in {}/.git/".format(os.getcwd()), fg=typer.colors.GREEN)
+    typer.secho("Initialized empty Pit repository in {}/.git/".format(target_dir), fg=typer.colors.GREEN)
