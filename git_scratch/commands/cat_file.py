d3013ff488acb0d7ca0b830f6bed17f758234843
Devanandhan <d.codandabany@gmail.com>
2025-06-30 22:40:04 +0200

    feat adapted Cat-file to main logics

--- git_scratch/commands/cat_file.py
+++ git_scratch/commands/cat_file.py
@@ -1,4 +1,3 @@
-
 import typer
 from git_scratch.utils.read_object import read_object
 
@@ -36,37 +35,19 @@ def cat_file(
     if len(oid) != 40 or not all(c in "0123456789abcdef" for c in oid.lower()):
         error(f"Invalid OID format: {oid}")
 
-    obj_path = os.path.join(".git", "objects", oid[:2], oid[2:])
-    if not os.path.exists(obj_path):
-        typer.secho(f"Error: Object {oid} not found in .git/objects.", fg=typer.colors.RED)
-        raise typer.Exit(code=1)
-
-    with open(obj_path, "rb") as f:
-        compressed_data = f.read()
     try:
-        full_data = zlib.decompress(compressed_data)
-    except zlib.error:
-        typer.secho("Error: Failed to decompress Git object.", fg=typer.colors.RED)
-        raise typer.Exit(code=1)
-
-    try:
-        header_end = full_data.index(b'\x00')
-        header = full_data[:header_end].decode()
-        content = full_data[header_end + 1:]
-        obj_type, size_str = header.split()
-        size = int(size_str)
+        obj_type, content = read_object(oid)
+    except FileNotFoundError:
+        error(f"Object {oid} not found.")
     except Exception:
-        typer.secho("Error: Invalid Git object format.", fg=typer.colors.RED)
-        raise typer.Exit(code=1)
-
-    if len(content) != size:
-        typer.secho("Error: Size mismatch in object.", fg=typer.colors.RED)
-        raise typer.Exit(code=1)
+        error("Failed to read Git object.")
 
     if type_opt:
         typer.echo(obj_type)
     elif pretty:
-        if obj_type != "blob":
-            typer.secho(f"Error: Pretty-print not supported for object type '{obj_type}'.", fg=typer.colors.RED)
-            raise typer.Exit(code=1)
-        typer.echo(content.decode(errors="replace"))
+        if obj_type in {"blob", "commit"}:
+            typer.echo(content.decode(errors="replace"))
+        elif obj_type == "tree":
+            pretty_print_tree(content)
+        else:
+            error(f"Pretty-print not supported for object type '{obj_type}'.")
